e03efffb421508f33eb743216df7b11b951cc80d
luis5944 <63348409+luis5944@users.noreply.github.com>
2021-02-14 20:12:09 +0100

    Update NumerosRepository.java

--- src/main/java/com/luisfn/comic/repositories/NumerosRepository.java
+++ src/main/java/com/luisfn/comic/repositories/NumerosRepository.java
@@ -12,5 +12,8 @@ import com.luisfn.comic.entities.Numero;
 public interface NumerosRepository extends JpaRepository<Numero, Long> {
 	@Query(value = "select * from numeros n natural join colecciones order by fecha_adquisicion desc", nativeQuery = true)
 	List<Numero> ultimosNumerosAdquiridos();
+	
+	@Query(value = "select * from numeros n natural join colecciones order by precio desc", nativeQuery = true)
+	List<Numero> numerosMasCaros();
 
 }
